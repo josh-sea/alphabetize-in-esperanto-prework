399c323de834ecbf1005c04e0294731988e4d177
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 10:04:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def alphabetize(array)
   # code here
-  esp_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split
+  esp_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
   binding.pry
   sort = array.sort_by do |word|
     word.split.map do |letter|
