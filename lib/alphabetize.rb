58c4cc3b7a62e5415e3d1306e4511d6cc63ab77d
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 10:05:23 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,8 +3,30 @@ def alphabetize(array)
   # code here
   esp_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
   #binding.pry
-  
-  
+#   ["a",
+# "b",
+# "c",
+# "ĉ",
+# "d",
+# "e",
+# "f",
+# "g",
+# "ĝ",
+# "h",
+# "ĥ",
+# "i",
+# "j",
+# "ĵ",
+# "k",
+# "l",
+# "m",
+# "n",
+# "o",
+# "p",
+# "r",
+# "s",
+# "ŝ",
+# ]
   sort = array.sort_by do |word|
     word.split.map do |letter|
       esperanto_alph_array.index(letter)
