64fdfac0f5187b93d9e4c87089088bca0bdfc002
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 10:01:51 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 def alphabetize(arr)
   # code here
-end
-
- def alphabetize(arr)
-  # code here
-end 
-   esperanto_alph_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+ esperanto_alph_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
    sorted = arr.sort_by do |word|
   word.split('').map do |letter|
     esperanto_alph_array.index(letter)
   end
    end
 sorted
-end
+endend
+
+ def alphabetize(arr)
+  # code here
+end 
+  
