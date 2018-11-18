7d6bfa87383b5f3b0c78ce516ea39f63adc45019
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 10:05:06 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,9 @@ require 'pry'
 def alphabetize(array)
   # code here
   esp_array = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
-  binding.pry
+  #binding.pry
+  
+  
   sort = array.sort_by do |word|
     word.split.map do |letter|
       esperanto_alph_array.index(letter)
